77fbc8cbd5970b3ec6298bb22e17336c3f1024d4
julianjackson <20231881+isjulianj@users.noreply.github.com>
2025-12-30 22:31:03 +0000

    updated spacing and added missing --------- Character Count -------

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ func main() {
 	fmt.Println("============ BOOKBOT ============")
 	fmt.Println("Analyzing book found at books/frankenstein.txt...")
 	fmt.Println("----------- Word Count ----------")
-	fmt.Printf("Found %d total words", countWords(text))
+	fmt.Printf("Found %d total words\n", countWords(text))
+	fmt.Println("--------- Character Count -------")
 	for _, c := range sortedInts {
 		fmt.Printf("%s : %d\n", c.character, c.count)
 	}
